6d21264f2b4ee23418848bb89118fdf973537391
Bruno Kantura <bruno.kantura@gmail.com>
2019-10-31 12:02:53 +0100

    test: added more middleware unit tests

--- test/middleware/middleware.test.ts
+++ test/middleware/middleware.test.ts
@@ -1,16 +1,12 @@
 import request from 'supertest';
-import express, { Request, Response, NextFunction } from 'express';
-import { errorMiddleware } from '../../src/middleware';
+import express from 'express';
+import { errorMiddleware, asyncMiddleware } from '../../src/middleware';
 import { NotFoundError } from '../../src/errors';
 
 describe('Forklift error middleware', () => {
   it('should handle forklift errors and have meta by default', async () => {
     const testController = {
-      get: (): ((
-        req: Request,
-        res: Response,
-        next: NextFunction,
-      ) => Promise<void>) => (): Promise<void> => {
+      get: () => (): Promise<void> => {
         throw new NotFoundError('Hey! Not found!');
       },
     };
@@ -28,11 +24,7 @@ describe('Forklift error middleware', () => {
 
   it('should disable meta trace log', async () => {
     const testController = {
-      get: (): ((
-        req: Request,
-        res: Response,
-        next: NextFunction,
-      ) => Promise<void>) => (): Promise<void> => {
+      get: () => (): Promise<void> => {
         throw new NotFoundError('Hey! Not found!');
       },
     };
@@ -50,11 +42,7 @@ describe('Forklift error middleware', () => {
 
   it('should handle generic errors gracefully', async () => {
     const testController = {
-      get: (): ((
-        req: Request,
-        res: Response,
-        next: NextFunction,
-      ) => Promise<void>) => (): Promise<void> => {
+      get: () => (): Promise<void> => {
         throw new Error('Hey! Not found!');
       },
     };
@@ -67,5 +55,32 @@ describe('Forklift error middleware', () => {
 
     expect(response.body.stack).toBeDefined();
     expect(response.body.name).toBe('Error');
+  });
+
+  it('should handle unhandled exceptions in handlers with asyncMiddleware', async () => {
+    function sleep(ms: number): Promise<void> {
+      return new Promise((resolve: Function): number =>
+        setTimeout(resolve, ms),
+      );
+    }
+
+    const testController = {
+      get: (): ((
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+      ) => Promise<void>) =>
+        asyncMiddleware(async () => {
+          await sleep(100);
+          throw new Error('Error');
+        }),
+    };
+    const app = express();
+    app.get('/get', testController.get());
+    app.use(errorMiddleware(false));
+
+    const response = await request(app).get('/get');
+
+    expect(response.body).toMatchSnapshot();
   });
 });
