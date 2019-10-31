311283fd1b5fad3267f482e38178df19bbf13d84
Bruno Kantura <bruno.kantura@gmail.com>
2019-10-31 12:03:12 +0100

    test: added errors module unit tests

---
+++ test/errors/errors.test.ts
@@ -0,0 +1,19 @@
+import { ForkliftError } from '../../src/errors';
+
+describe('Forklift error module', () => {
+  it('should provide easy extension of ForkliftError', () => {
+    class CustomError extends ForkliftError {
+      constructor(message: string) {
+        super(message, 999, "Custom error")
+      }
+    }
+
+    const test = () => { throw new CustomError("Some test message")};
+
+    try {
+      test();
+    } catch(e){
+      expect(e.toJson(false)).toMatchSnapshot();
+    }
+  });
+});
