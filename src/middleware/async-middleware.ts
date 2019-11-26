13423e5c2cfe7b6cfc90a02a82dcebdc3c938c3d
Bruno Kantura <bruno.kantura@gmail.com>
2019-11-26 17:01:43 +0100

    fix(middleware): rewritten types for asyncMiddleware

--- src/middleware/async-middleware.ts
+++ src/middleware/async-middleware.ts
@@ -7,21 +7,13 @@ import { Request, Response, NextFunction } from 'express';
  * provides a rejected promise, it will be caught and sent to the next function
  * to be handled inside an error handling middleware. This wrapper is useful
  * when using async/await since in those cases default error middleware won't
- * catch rejected promises and you are required to wrap your logic inside try 
+ * catch rejected promises and you are required to wrap your logic inside try
  * catch blocks.
  */
 
 export const asyncMiddleware = (
-  fn: Function,
-): ((
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => Promise<void>) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => {
+  fn: (req: Request, res: Response, next?: NextFunction) => Promise<void>,
+) => async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     await fn(req, res);
   } catch (err) {
